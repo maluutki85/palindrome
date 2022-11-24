50d3ec16c710a54f083b51a1ab24048f28c0940f
Jakub Dziurgot <j.dziurgot@heating.pl>
2022-11-24 15:32:12 +0100

    Add documentation

--- palindrome.py
+++ palindrome.py
@@ -5,3 +5,10 @@ def chcek_palindrome(x):
         return "False"
 x = input("Wprowdź słowo:")
 print(chcek_palindrome(x))
+
+'''
+Program pozwalający sprawdzić czy wyraz
+jest palindromem. Fukncja składa się 
+z jednego argumentu, który podawany 
+jest przez input
+'''
